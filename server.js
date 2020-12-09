5f2c095bebdd692029327396538fff8ef8c17ee7
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-12-08 21:30:54 -0500

    correcting spelling errors in server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.listen(3000, function*(){
-    console,log('Books in Space API is now listening on port 3000...');
+app.listen(3000, function(){
+    console.log('Books in Space API is now listening on port 3000...');
 })
