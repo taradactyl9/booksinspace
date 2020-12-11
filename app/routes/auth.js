792a4cda2b813dc6c7cc67cef59708e40b950a9c
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-12-10 21:40:51 -0500

    added .env config

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,2 +1,17 @@
 const express = require('express');
+const router = express.Router();
 const passport = require('passport');
+
+app.get('/github', passport.authenticate('github'));
+
+router.get('/githun/callback', passport.authenticate('github', {failureRedirect: '/login-failed'}),
+function (req, res) {
+    res.redirect('/models/index.js');
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/logged-out');
+})
+
+module.exports = router;
