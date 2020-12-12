c1263c5a9408b05e75b63f69002e9b9fd627fc20
Tara Davies <taradavies@Taras-Air.attlocal.net>
2020-12-12 12:33:49 -0500

    adding initial controller efforts

---
+++ app/controllers/controller.js
@@ -0,0 +1,60 @@
+const db = require("../models");
+/* const Books = db.booksdb;
+const Reviews = db.booksdb;
+const Shelves = db.booksdb;
+const Users = db.booksdb;
+const Wanttoread = db.booksdb; */
+const Op = db.Sequelize.Op;
+
+//Books Table
+// Select a book then create a book id
+exports.create = (req, res) => {
+    db.books.findorCreate
+};
+
+// Mark it as Read/Want to read
+exports.update = (req, res) => {
+  
+};
+
+// Rate a book 1st time
+exports.create = (req, res) => {
+  
+};
+
+// Change a book rating
+exports.update = (req, res) => {
+  
+};
+
+// Review a book 
+exports.create = (req, res) => {
+  
+};
+
+// Change a book review
+exports.update = (req, res) => {
+  
+};
+
+// Add a book read date
+exports.create = (req, res) => {
+  
+};
+
+// Change a book read date
+exports.update = (req, res) => {
+  
+};
+
+// Remove a book from shelf
+exports.delete = (req, res) => {
+  
+};
+
+//Shelves
+// user adds a book to Read/want to read - create shelves, create shelf id
+exports.create = (req, res) => {
+  
+};
+
--- models/index.js
+++ models/index.js
@@ -19,6 +19,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.booksdb = require("./users.js")(sequelize, Sequelize);
+db.booksdb = require("../models")(sequelize, Sequelize);
 
 module.exports = db;
