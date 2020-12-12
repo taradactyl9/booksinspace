6f14318439bb80773c6bc84d25e420d54e33daef
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-12-12 11:18:15 -0500

    adding placeholder routes

---
+++ app/routes/routes.js
@@ -0,0 +1,16 @@
+module.exports = app => {
+    const books = require("..controllers/controller.js"); //Placeholder
+
+    var router = require("express").Router();
+
+    //Retrieve your Shelf - GET Request
+
+    //Add to shelf as want to read - POST route
+
+    //Mark as Read - Patch Request
+
+    //Rate a book - Patch Request
+
+    //Add written review - Patch request
+
+}
