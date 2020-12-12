988428f6f9703bf0f9e51180b7968e475657c2fc
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-12-12 12:17:09 -0500

    added placeholder routes to the route.js file

---
+++ app/controllers/controller.js
@@ -0,0 +1,13 @@
+ //THESE ARE THE ROUTES THAT NEED CONTROLLERS
+ 
+    //Add a book - POST Request
+    
+    //Retrieve your Shelf - GET Request
+
+    //Add to shelf as want to read - POST route
+
+    //Mark as Read - Patch Request
+
+    //Rate a book - Patch Request
+
+    //Add written review - Patch request
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,16 +1,34 @@
-module.exports = app => {
-    const books = require("..controllers/controller.js"); //Placeholder
+const express = require('express');
+const router = require("express").Router();
+const controller = require("..controllers/controller.js"); //Placeholder
 
-    var router = require("express").Router();
+
+    //Add a book - POST Request
+    
+    router.post('/:id/', controller.create); //PLACEHOLDER 
 
     //Retrieve your Shelf - GET Request
 
+    router.get("/", controller.findAll); // PLACEHOLDER
+
     //Add to shelf as want to read - POST route
+
+    router.post("/", controller.update); //PLACEHOLDER
 
     //Mark as Read - Patch Request
 
+    router.patch("/", controller.update); //PLACEHOLDER
+
     //Rate a book - Patch Request
+
+    router.patch("/", contoller.update);
 
     //Add written review - Patch request
 
-}
+    router.patch("/", controller.update);
+
+    //Delete a book from shelf - delete request
+
+    router.delete("/", controller.delete);
+
+    
