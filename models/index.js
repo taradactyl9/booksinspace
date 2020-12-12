43334846f727d0cbca786f44e1d95ec46ac4ef85
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-12-12 13:42:59 -0500

    fixing server

--- models/index.js
+++ models/index.js
@@ -3,14 +3,14 @@ const dbConfig = require("../config/config.json");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+  dialect: "postgres", //dbConfig.dialect,
   operatorsAliases: false,
 
   pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
+    max: 5, //dbConfig.pool.max,
+    min: 0, //dbConfig.pool.min,
+    acquire: 30000, //dbConfig.pool.acquire,
+    idle: 10000 //dbConfig.pool.idle
   }
 });
 
@@ -19,6 +19,10 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.booksdb = require("../models")(sequelize, Sequelize);
+db.books = require("../models/books.js")(sequelize, Sequelize);
+db.reviews = require("../models/reviews.js")(sequelize, Sequelize);
+db.shelves = require("../models/shelves.js")(sequelize, Sequelize);
+db.users = require("../models/users.js")(sequelize, Sequelize);
+db.wanttoread = require("../models/wanttoread.js")(sequelize, Sequelize);
 
 module.exports = db;
