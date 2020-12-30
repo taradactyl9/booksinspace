8855df3e10dc3e01ff0190b486c26cc782aac9fa
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-12-29 20:49:49 -0500

    adjust formatting

--- app/controllers/booksController.js
+++ app/controllers/booksController.js
@@ -2,9 +2,7 @@ const db = require("../../models");
 const Books = db.Books;
 const Sequelize = require('sequelize');
 
-// const Op = db.Sequelize.Op;
-// db.books.belongsTo(db.users);
-// var primaryId = 1;
+//Home page
 
 exports.bookHome = (req, res) => {
     res.render('layouts/index');
@@ -32,30 +30,6 @@ const newBook = db.Books.create({
 
 }
 
-// Save book to shelves on POST - Tested and Working
-exports.books_create_post = function(req, res) {
-    if (!req.body.title) {
-        res.status(400).send({
-            message: "Book can not be empty!"
-        });
-        return;
-    }
-
-    const { title, user_id, date_read, status, has_read } = req.body;
-
-    const newBook = db.Books.create({
-        title,
-        user_id,
-        date_read,
-        status,
-        has_read
-    });
-
-    res.send(
-        'This book has been added!'
-    )
-};
-
 // Update a Book
 
 exports.books_update_patch = function(req, res) { // TESTED & WORKING
@@ -70,7 +44,7 @@ exports.books_update_patch = function(req, res) { // TESTED & WORKING
     res.send('This book is updated');
 };
 
-// Delete a book from shelf
+// Delete a book 
 
 exports.book_delete_post = function(req, res) {
   const { id } = req.params;
@@ -83,111 +57,3 @@ exports.book_delete_post = function(req, res) {
   res.json(deletedBook)
 };
  
-// exports.shelves_controller.shelves_findAll_get = function(req, res) {
-//   const { user_id } = req.params;
-
-//   db.Books.
-// }
-
-// // Rate a book 1st time on POST
-// exports.rate_create_post = function(req, res) {
-//   if (!req.body.status) {
-//     res.status(400).send({
-//         message: "No rating selected"
-//     });
-//     return;
-//   }
-
-//   const { title } = req.body;
-//   const { user_id } = req.body;
-//   const { date_read } = req.body
-//   const { status } = req.body
-
-
-//   const newBook = db.Books.post({
-//    title,
-//    user_id,
-//    date_read,
-//    status
-//   })
-//   res.send(
-//     'This book has been added to shelf!'
-//   )
-//   };
-
-// // Change a book rating on UPDATE
-// exports.rating_update_patch = function(req, res) {
-//   if (!req.body.status) {
-//     res.status(400).send({
-//         message: "No rating selected"
-//     });
-//     return;
-//   }
-
-//   const { title } = req.body;
-//   const { user_id } = req.body;
-//   const { date_read } = req.body
-//   const { status } = req.body
-
-
-//   const newBook = db.Books.update({
-//    title,
-//    user_id,
-//    date_read,
-//    status
-//   })
-//   res.send(
-//     'Book has been updated.'
-//   )
-
-// };
-
-// // Review a book on POST
-// exports.review_create_post = function(req, res) {
-//   if (!req.body.status) {
-//     res.status(400).send({
-//         message: "No review saved."
-//     });
-//     return;
-//   }
-
-//   const { title } = req.body;
-//   const { user_id } = req.body;
-//   const { date_read } = req.body
-//   const { status } = req.body
-
-
-//   const newBook = db.Books.create({
-//    title,
-//    user_id,
-//    date_read,
-//    status
-//   })
-//   res.send(
-//     'This review has been saved.'
-//   )
-//   };
-
-
-
-
-
-
-
-
-
-
-
-//  //THESE ARE THE ROUTES THAT NEED CONTROLLERS
-
-//     //Add a book - POST Request
-
-//     //Retrieve your Shelf - GET Request
-
-//     //Add to shelf as want to read - POST route
-
-//     //Mark as Read - Patch Request
-
-//     //Rate a book - Patch Request
-
-//     //Add written review - Patch
