e3458566c8fc2031e1b453f056485a5cdd7ca4aa
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-12-29 20:44:35 -0500

    create and test user update controller

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -26,23 +26,20 @@ exports.user_create_post = function(req, res) {
     )
 }
 
-// // user logs in 1st - create login date POST
-// exports.login_create_post = function(req, res) {
-//   res.send('NOT IMPLEMENTED: login date created')
-// };
+exports.user_update_patch = function(req, res) {
+    // if (!req.body.username || !req.body.password) {
+    //     res.status(400).send({
+    //         message: "Need username and password"
+    //     })
+    // return;
+    // }
+    const { id } = req.params;
 
-// // user logs in - update login date PATCH
-// exports.login_update_patch = function(req, res) {
-//   res.send('NOT IMPLEMENTED: user login date update PATCH')
-// };
+    const updatedUser = db.User.update(req.body, {
+        where: {
+            id
+        }
+    })
 
-
-// // user deletes acct POST
-// exports.user_deleteAll_post = function(req, res) {
-//   res.send('NOT IMPLEMENTED: user deletes acct POST')
-// };
-
-// // user visits profile page - retrieve user library GET
-// exports.user_retrieveAll_get = function(req, res) {
-//   res.send('NOT IMPLEMENTED: user retrieves all books for profile page GET')
-// }
+    res.send('Your user has been updated');
+}
